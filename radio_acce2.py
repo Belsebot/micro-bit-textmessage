9aae1588b69cc51fc3cf577ffc4db23f7693c2e1
Belsebot <belse.bubba@gmail.com>
2017-12-17 12:51:35 +0200

    Update radio_acce2.py

--- radio_acce2.py
+++ radio_acce2.py
@@ -4,12 +4,12 @@ from microbit import *
 radio.on()
 display.show("R")
 
-while True:
+while True:                 #main loop
     display.show("R")
-    msg=radio.receive()
+    msg=radio.receive()     #receive message
     
-    if (msg!=None):
+    if (msg!=None):         #if message is something else than none show current message
         display.show(msg,delay=400,wait=True,loop=False,clear=True)
-        print (msg)
+        print (msg)         #debug
     
     
